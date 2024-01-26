e2205d53b7a16b86522e7f2161e7ae8888ef849a
ydy_dreemurr <ydy_dreemurr@outlook.com>
2024-01-26 11:56:47 +0800

    修复了物品和附魔分开判断的逻辑错误

--- src/main/java/hszy/ydy/sekiro/MyListener.java
+++ src/main/java/hszy/ydy/sekiro/MyListener.java
@@ -5,18 +5,13 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 import java.util.List;
@@ -61,72 +56,63 @@ public class MyListener implements Listener {
         }
     }
 
+    /*检测物品*/
+    public static boolean checkItem(ItemStack item,Material target){
+        String ench_s = Sekiro.enchantment;
+        int lvl = Sekiro.enchantedlvl;
+        boolean f1,f2;
+        //检测附魔
+        Enchantment ench = Enchantment.getByKey(NamespacedKey.minecraft(ench_s));
+        f1 = (ench==null||(item.getEnchantmentLevel(ench)==lvl));
+        //检测物品类型
+        f2 = (item.getType() == target);
+
+        return f1&&f2;
+    }
+
     @EventHandler
     public void onAttack(EntityDamageByEntityEvent e)// 监听实体攻击事件
     {
         if(e.getEntity() instanceof Player){//是玩家
             Player p1 = (Player) e.getEntity();
             Entity p2 = e.getDamager();
-            ItemStack shield_1,shield_2;
-            shield_1=new ItemStack(Material.SHIELD);
-            shield_2=new ItemStack(Material.SHIELD);
-            boolean have_shield=false;
-            if(p1.getInventory().getItemInMainHand().getType() == Material.SHIELD){
-                shield_1=p1.getInventory().getItemInMainHand();
-                have_shield=true;
-            }
-            if(p1.getInventory().getItemInOffHand().getType() ==Material.SHIELD){
-                shield_2=p1.getInventory().getItemInOffHand();
-                have_shield=true;
-            }
-            //检测手里盾的附魔
-            if(have_shield)
+            ItemStack mainHand = p1.getInventory().getItemInMainHand();
+            ItemStack offHand = p1.getInventory().getItemInOffHand();
+
+            boolean flag_=(checkItem(mainHand,Material.SHIELD)||checkItem(offHand,Material.SHIELD));
+
+            //检测手里物品
+            if(flag_)
             {
-                boolean flag_ = false;//检测是否需要盾上有指定附魔
-                if(Sekiro.enchantment.equals("")){flag_=true;}
-                else {
-                    Enchantment ench = Enchantment.getByKey(NamespacedKey.minecraft(Sekiro.enchantment));
-                    if(Sekiro.enchantedlvl >= 0) {
-                        if ((shield_1.getEnchantmentLevel(ench) == Sekiro.enchantedlvl) || shield_2.getEnchantmentLevel(ench) == Sekiro.enchantedlvl) {
-                            flag_ = true;
+                double dmg = (e.getDamage());
+                dmg = dmg<0?dmg*(-1):dmg;
+                //flag负责限定弹反范围
+                boolean flag = isAllowBlock(p1.getLocation(),p2.getLocation(),p1.getEyeLocation(),Sekiro.angle);
+                //弹反成功
+                if(p1.isHandRaised() && !p1.isBlocking() && flag)
+                {
+                    ((Damageable) p2).damage(dmg * Sekiro.magnification,p1);
+                    p1.playSound(p1.getLocation(), Sound.ENTITY_ITEM_BREAK,(float) 1.0,(float) 1);
+                    if(p2 instanceof Player)((Player) p2).playSound(p2.getLocation(), Sound.ENTITY_ITEM_BREAK,(float) 1.0,(float) 1);
+                    e.setCancelled(true);
+                }/*格挡成功*/
+                else if(p1.isBlocking()){
+                    double punishdmg=dmg*punish(p1.getLocation(),p2.getLocation(),p1.getEyeLocation());
+                    //总不能格挡的时候给震死吧
+                    if(e.getFinalDamage()<0.1) {//挡住了(两种情况)
+                        if (p1.getHealth()-punishdmg > 0) {
+                            p1.setHealth(p1.getHealth()-punishdmg);
                         }
-                    }else {
-                        if ((shield_1.getEnchantmentLevel(ench) >= 1) || (shield_2.getEnchantmentLevel(ench) >= 1)) {
-                            flag_ = true;
-                        }
-                    }
-                }
-                if(flag_){
-                    double dmg = (e.getDamage());
-                    dmg = dmg<0?dmg*(-1):dmg;
-                    //flag负责限定弹反范围
-                    boolean flag = isAllowBlock(p1.getLocation(),p2.getLocation(),p1.getEyeLocation(),Sekiro.angle);
-                    //弹反成功
-                    if(p1.isHandRaised() && !p1.isBlocking() && flag)
-                    {
-                        ((Damageable) p2).damage(dmg * Sekiro.magnification,p1);
-                        p1.playSound(p1.getLocation(), Sound.ENTITY_ITEM_BREAK,(float) 1.0,(float) 1);
-                        if(p2 instanceof Player)((Player) p2).playSound(p2.getLocation(), Sound.ENTITY_ITEM_BREAK,(float) 1.0,(float) 1);
                         e.setCancelled(true);
-                    }/*格挡成功*/
-                    else if(p1.isBlocking()){
-                        double punishdmg=dmg*punish(p1.getLocation(),p2.getLocation(),p1.getEyeLocation());
-                        //总不能格挡的时候给震死吧
-                        if(e.getFinalDamage()<0.1) {//挡住了(两种情况)
-                            if (p1.getHealth()-punishdmg > 0) {
-                                p1.setHealth(p1.getHealth()-punishdmg);
-                            }
-                            e.setCancelled(true);
-                        }else {//没挡住(背后)
-                            e.setDamage(punishdmg);
-                            //低血量特效
-                            weiordie(p1,p2,p1.getHealth() - e.getFinalDamage());
-                        }
-                    }
-                    else {
+                    }else {//没挡住(背后)
+                        e.setDamage(punishdmg);
                         //低血量特效
                         weiordie(p1,p2,p1.getHealth() - e.getFinalDamage());
                     }
+                }
+                else {
+                    //低血量特效
+                    weiordie(p1,p2,p1.getHealth() - e.getFinalDamage());
                 }
             }
         }
